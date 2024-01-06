45f04dc3924b6fce5e2fda72a4d75190626a2688
RO35ERT <trebrorobertsbob@gmail.com>
2024-01-06 23:56:30 +0200

    added hero info

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,16 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+
 const Hero = () => {
     return ( 
         <div className="hero">
-            
+            <h1>Tax Information</h1>
+            <div className="hero-details">
+                <h1>What would tou like to do</h1>
+                <select name="" id="">
+                    <option value="">select option</option>
+                </select>
+                <Link>Get started</Link>
+            </div>
         </div>
      );
 }

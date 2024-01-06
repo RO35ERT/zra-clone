b592c843bfdabbdebed2c6b93f31b4b2d9151e7c
RO35ERT <trebrorobertsbob@gmail.com>
2024-01-07 00:50:48 +0200

    added structure and contents

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,8 +1,20 @@
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
 const Footer = () => {
     const year = new Date().getFullYear();
     return ( 
         <div className="footer">
-            
+           <div className="footer-details">
+                <p>&copy; {year} Zambia Revenue Authority. All rights reserved</p>
+            </div> 
+            <div className="links">
+                <ul>
+                    <li><Link><i className="fa fa-facebook"></i></Link></li>
+                    <li><Link><i className="fa fa-twitter"></i></Link></li>
+                    <li><Link><i className="fa fa-instagram"></i></Link></li>
+                    <li><Link><i className="fa fa-linkedin"></i></Link></li>
+                    <li><Link><i className="fa fa-youtube"></i></Link></li>
+                </ul>
+            </div>
         </div>
      );
 }

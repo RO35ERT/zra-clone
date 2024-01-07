91b38fbe2c6e3b460cfe99ab4e8414adde157878
RO35ERT <trebrorobertsbob@gmail.com>
2024-01-07 14:37:36 +0200

    'align the hero section'

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,13 +1,18 @@
-import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import { Link } from "react-router-dom";
 
 const Hero = () => {
     return ( 
         <div className="hero">
             <h1>Tax Information</h1>
             <div className="hero-details">
-                <h1>What would tou like to do</h1>
+                <h2>What would tou like to do</h2>
                 <select name="" id="">
                     <option value="">select option</option>
+                    <option value="">Register for a TPIN</option>
+                    <option value="">File Returns</option>
+                    <option value="">Pay Taxes Online</option>
+                    <option value="">Apply for Tax Compliance Certificate</option>
+                    <option value="">Import vehicle</option>
                 </select>
                 <Link>Get started</Link>
             </div>
